ac9c875abacd9cfe1a117e3a5ba5c45aaf7649b9
Lauren Liu <liulj2@mcmaster.ca>
2017-05-17 11:42:30 -0400

    Updated inputs

--- deterministic.sh
+++ deterministic.sh
@@ -1,25 +1,29 @@
 ##INPUTS
 
-bvecFile=SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct.bvec
-bvalFile=SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct.bval
-multiVol=SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct.nii.gz
-b0Mask=SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet_mask.nii.gz
-b0Brain=SPN01_CMH_0001_01_01_DTI60-1000_20_Ax-DTI-60plus5_eddy_correct_b0_bet.nii.gz
+bvecFile=SPN01_CMH_P001_01_01_DTI60-1000_17_Ax-DTI-60plus5_eddy_correct.bvec
+bvalFile=SPN01_CMH_P001_01_01_DTI60-1000_17_Ax-DTI-60plus5_eddy_correct.bval
+multiVol=SPN01_CMH_P001_01_01_DTI60-1000_17_Ax-DTI-60plus5_eddy_correct.nii.gz
+b0Mask=SPN01_CMH_P001_01_01_DTI60-1000_17_Ax-DTI-60plus5_eddy_correct_b0_bet_mask.nii.gz
+b0Brain=SPN01_CMH_P001_01_01_DTI60-1000_17_Ax-DTI-60plus5_eddy_correct_b0_bet.nii.gz
+T1wBrain=T1w_brain.nii.gz
+wmParc=wmparc.nii.gz
+mniBrain=MNI152_T1_2mm_brain.nii.gz
+shenAtlas=shen_2mm_268_parcellation.nii.gz
 
 #get T1w_brain, wmparc, shen, MNI
 
 echo "making scheme file"
 fsl2scheme -bvecfile ${bvecFile} -bvalfile ${bvalFile} > A.scheme
 echo "registering b0-T1"
-flirt -in ${b0Brain} -ref T1w_brain.nii.gz -omat tfm.mat
+flirt -in ${b0Brain} -ref ${T1wBrain} -omat tfm.mat
 convert_xfm -omat tfm_invert.mat -inverse tfm.mat
 echo "registering white matter mask"
-flirt -in wmparc.nii.gz -ref ${b0Brain} -applyxfm -init tfm_invert.mat -o wmparc_invert.nii.gz
+flirt -in ${wmParc} -ref ${b0Brain} -applyxfm -init tfm_invert.mat -o wmparc_invert.nii.gz
 fslmaths wmparc_invert.nii.gz -thr 2500 -bin wmparc_invert_bin.nii.gz
 echo "registering MNI"
-flirt -in MNI152_T1_2mm_brain.nii.gz -ref ${b0Brain} -interp nearestneighbour -omat mni.mat
+flirt -in ${mniBrain} -ref ${b0Brain} -interp nearestneighbour -omat mni.mat
 echo "registering atlas"
-flirt -in shen_2mm_268_parcellation.nii.gz -ref ${b0Brain} -interp nearestneighbour -applyxfm -init mni.mat -o atlas.nii.gz
+flirt -in ${shenAtlas} -ref ${b0Brain} -interp nearestneighbour -applyxfm -init mni.mat -o atlas.nii.gz
 
 #***************************************************************************
 #DETERMINISTIC TRACTOGRAPHY
@@ -35,14 +39,14 @@ track -inputfile wdt.nii.gz -inputmodel dt -seedfile wmparc_invert_bin.nii.gz -c
 fa -inputfile wdt.nii.gz -outputfile fa.nii.gz
 
 echo "calculating connectivity matrix"
-conmat -inputfile allEndPointTracts.Bfloat -targetfile atlas.nii.gz -scalarfile fa.nii.gz -tractstat min -outputroot final_
+conmat -inputfile allEndPointTracts.Bfloat -targetfile atlas.nii.gz -scalarfile fa.nii.gz -tractstat min -outputroot conmat_
 
 #***************************************************************************
 #visuals
 vtkstreamlines -colourorient < allEndPointTracts.Bfloat > allEndPointTracts.vtk
-# paraview allEndPointTracts.vtk
+paraview allEndPointTracts.vtk
 
 matlab
-conmat_path = 'final_ts.csv';
+conmat_path = 'conmat_ts.csv';
 myconmat = csvread(conmat_path, 1, 0);
 figure, imagesc(myconmat)
