1786d387419d3c78cdbdf2ce8bebc5b076dce686
Sonja Stojanovski <sstojanovski@cajal.camhres.ca>
2018-02-05 16:37:59 -0500

    chaning original to be applicable to PNC_dti_conn

--- pipelining/connectivityPipeline.sh
+++ pipelining/connectivityPipeline.sh
@@ -6,21 +6,23 @@ module load python
 module load AFNI
 
 #-------------------------------------------------------------------------------
+# ADD USAGE FOR Projectname, project path (where raw data is)
 #-------------------------------------------------------------------------------
 # enter project directory
-projectName=SPINS
+projectName=PNC_dti_conn
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-projectPath=/archive/data-2.0/${projectName}/data/nii/
-projectDir=/scratch/lliu/${projectName}/
+projectPath=/projects/sstojanovski/${projectName}/bedpostX/data/
+projectDir=/projects/sstojanovski/${projectName}/
 
 if [ ! -d "${projectDir}" ]
 	then
-	cd /scratch/lliu/ | mkdir ${projectDir}
+	cd /projects/sstojanovski/ | mkdir ${projectDir}
 	cd ${projectDir}
 
 	mkdir bedpostX
 	mkdir conmat
+	mkdir Qlogs
 fi
 
 cd ${projectPath}
@@ -28,9 +30,7 @@ cd ${projectPath}
 for subjectID in *
 do
 	tempSubjDir=/scratch/lliu/tmp/${subjectID}/
-	if [[ ! ${subjectID} == *PHA* ]]
-		then
-			qsub -V -e /scratch/lliu/QLogs/ -o /scratch/lliu/QLogs/ -v projName=${projectName} -v subjID=${subjectID} /scratch/lliu/ConnectivityPipe/runThis.sh
-			# python /scratch/lliu/ConnectivityPipe/connectivityPipeline.py ${projectName} ${subjectID}
+		qsub -V -e {$projectDir}/QLogs/ -o {$projectDir}/QLogs/ -v projName=${projectName} -v subjID=${subjectID} {$projectDir}/scripts/runThis.sh
+		# python /scratch/lliu/ConnectivityPipe/connectivityPipeline.py ${projectName} ${subjectID}
 	fi
 done
